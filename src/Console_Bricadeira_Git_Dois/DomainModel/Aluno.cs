52b7ef243685cb63001198818db1964af7f3acd4
edmilson.hora <edmilson.hora@gpnet.com.br>
2024-02-23 10:57:53 -0300

    Adicionado Validação

--- src/Console_Bricadeira_Git_Dois/DomainModel/Aluno.cs
+++ src/Console_Bricadeira_Git_Dois/DomainModel/Aluno.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Console_Bricadeira_Git_Dois.DomainModel
 {
-    public class Aluno
+    public class Aluno : EntityBase
     {
         public string Nome { get; set; }
         public string RA { get; set; }
+
+        public override void Validar()
+        {
+            base.Validar();
+        }
     }
 
     public interface IAlunoRepository : IRepositoryBase<Aluno> { }
