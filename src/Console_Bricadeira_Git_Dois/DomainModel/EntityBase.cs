294fdecc81bed8f7184ecd0d7a4b46f2108c2d24
edmilsonhora <38793820+edmilsonhora@users.noreply.github.com>
2024-02-23 09:49:57 -0300

    DomainModel

---
+++ src/Console_Bricadeira_Git_Dois/DomainModel/EntityBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Bricadeira_Git_Dois.DomainModel
+{
+    public abstract class EntityBase
+    {
+        protected StringBuilder RegrasQuebradas = new StringBuilder();
+        public int Id { get; set; }
+
+        public virtual void Validar()
+        {
+            if(RegrasQuebradas.Length > 0)
+            {
+                throw new ApplicationException(RegrasQuebradas.ToString());
+            }
+        }
+    }
+}
---
+++ src/Console_Bricadeira_Git_Dois/DomainModel/Professor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Bricadeira_Git_Dois.DomainModel
+{
+    public class Professor
+    {
+        public string Nome { get; set; }
+        public string RE { get; set; }
+    }
+}
